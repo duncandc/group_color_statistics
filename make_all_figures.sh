7a51d8e581a7619da25d715e69c14a019f35e3d1
Duncan Campbell <duncan.alan.campbell@gmail.com>
2014-09-02 13:42:24 -0400

    added ait statments

--- make_all_figures.sh
+++ make_all_figures.sh
@@ -4,24 +4,31 @@
 
 #figure 1
 python mock_colors.py Mr19_age_distribution_matching_mock_sys_empty_shuffle_satrel_shuffle
+wait
 
 #figure 2
 #artwork made in keynote.
 
 #figure 3
-python mock_HOD_5.0.py Mr19_age_distribution_matching_mock_sys_empty_shuffle_satrel_shuffle
+python mock_groups_HOD.py Mr19_age_distribution_matching_mock_sys_empty_shuffle_satrel_shuffle
+wait
 
 #figure 4
 python mock_groups_f_L.py Mr19_age_distribution_matching_mock_sys_empty_shuffle_satrel_shuffle
+wait
 
 #figure 5
 python mock_groups_f_M.py Mr19_age_distribution_matching_mock_sys_empty_shuffle_satrel_shuffle
+wait
 
 #figure 6
-python mock_CLF_1.0.py Mr19_age_distribution_matching_mock_sys_empty_shuffle_satrel_shuffle
+python mock_groups_CLF.py Mr19_age_distribution_matching_mock_sys_empty_shuffle_satrel_shuffle
+wait
 
 #figure 7
 python conformity_w_errors_mock_runs_2.0.py Mr19_age_distribution_matching_mock Mr19_age_distribution_matching_mock_sys_empty_shuffle_satrel_shuffle
+wait
 
 #figure 8
 python HTP.py yang_mocks Mr19_age_distribution_matching_mock_sys_empty_shuffle_satrel_shuffle
+wait
